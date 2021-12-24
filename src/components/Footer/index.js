f8b89b63459691f28e4e7ca126196a9bb4aa0494
hanseulhee <3021062@gmail.com>
2021-12-24 19:12:19 +0900

    :lipstick:Feat: Nav, Footer styles, global css modify

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <div className={styles.footerContainer}>
-         <h2 className={styles.footerContainer__made}>Develop, Design by hanseulhee</h2>
+         <h2 className={styles.footerContainer__made}>Copyright © 주식회사 슬빙 All right reserved.</h2>
     </div>
   );
 }
